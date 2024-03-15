7ca2260a11e001887aefc9105c422d3e6efe6c9c
GensouSakuya <22719048+GensouSakuya@users.noreply.github.com>
2024-03-15 17:01:24 +0800

    support subscription of weibo

---
+++ src/GensouSakuya.KookBot/GensouSakuya.KookBot.App/BackgroundWorkers/WeiboWorker.cs
@@ -0,0 +1,156 @@
+﻿using GensouSakuya.KookBot.App.Handlers;
+using GensouSakuya.KookBot.App.Interfaces;
+using Kook;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace GensouSakuya.KookBot.App.BackgroundWorkers
+{
+    internal class WeiboWorker
+    {
+        readonly Func<ConcurrentDictionary<string, ConcurrentDictionary<string, WeiboSubscribeModel>>> _getSubscribeData;
+        readonly CancellationTokenSource _cancellationTokenSource;
+        readonly IKookService _kookService;
+        readonly ILogger _logger;
+        public WeiboWorker(IKookService kookService, ILogger logger, Func<ConcurrentDictionary<string, ConcurrentDictionary<string, WeiboSubscribeModel>>> getSubscribeData)
+        {
+            _kookService = kookService;
+            _logger = logger;
+            _lastWeiboId = new ConcurrentDictionary<string, string>();
+            _cancellationTokenSource = new CancellationTokenSource();
+            _getSubscribeData = getSubscribeData;
+            Task.Run(() => LoopCheck(_cancellationTokenSource.Token));
+        }
+
+        static Regex _faceRegex = new Regex("<span class=[\"']url-icon[\"']><img\\s[^>]*?alt=[\"']?([^>]+?)[\"']?\\s[^>]*?\\/?><\\/span>");
+        static Regex _newLineRegex = new Regex("<br\\s/>");
+        static Regex _fullTextRegex = new Regex("<a href=\"(.*?)\">全文<\\/a>");
+        private static TaskCompletionSource<bool> _completionSource = new TaskCompletionSource<bool>();
+        private ConcurrentDictionary<string, string> _lastWeiboId;
+        private async Task LoopCheck(CancellationToken token)
+        {
+            try
+            {
+                await Task.Delay(5000);
+                var loopSpan = new TimeSpan(0, 10, 0);
+                var intervalSpan = new TimeSpan(0, 0, 10);
+                var templateUrl = "https://m.weibo.cn/api/container/getIndex?type=uid&value={0}";
+                RestResponse homeres = null;
+                var cookieContainer = new CookieContainer();
+                while (!token.IsCancellationRequested)
+                {
+                    if (_completionSource.Task.IsCompleted)
+                        _completionSource = new TaskCompletionSource<bool>();
+                    var option = new RestClientOptions()
+                    {
+                        ConfigureMessageHandler = h =>
+                        {
+                            var handler = (HttpClientHandler)h;
+                            handler.CookieContainer = cookieContainer;
+                            handler.UseCookies = true;
+                            return handler;
+                        }
+                    };
+                    using (var client = new RestClient(option))
+                    {
+                        //try
+                        //{
+                        //    client.AddDefaultHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0");
+                        //    //set cookie
+                        //    homeres = await client.GetAsync(new RestRequest("https://live.douyin.com"));
+                        //}
+                        //catch (Exception e)
+                        //{
+                        //    _logger.LogError(e, "douyin refresh cookies error");
+                        //}
+                        var subscribers = _getSubscribeData();
+                        foreach (var room in subscribers)
+                        {
+                            if (room.Value.Count <= 0)
+                                continue;
+
+                            try
+                            {
+                                var url = string.Format(templateUrl, room.Key);
+                                var res = await client.GetAsync(new RestRequest(url));
+                                if (!res.IsSuccessStatusCode)
+                                {
+                                    _logger.LogError(res.ErrorException, "get roominfo failed");
+                                    continue;
+                                }
+                                var content = res.Content;
+                                var jobj = JObject.Parse(content);
+                                var name = jobj["data"]["userInfo"]["screen_name"];
+                                var containerid = jobj["data"]["tabsInfo"]["tabs"][1]["containerid"];
+                                res = await client.ExecuteGetAsync(new RestRequest(url + "&containerid=" + containerid));
+                                content = res.Content;
+                                jobj = JObject.Parse(content);
+                                var weibos = jobj["data"]["cards"];
+                                var newest = weibos[0];
+                                var id = newest["mblog"]["id"].ToString();
+                                var text = newest["mblog"]["text"].ToString();
+                                if (!_lastWeiboId.ContainsKey(room.Key))
+                                {
+                                    _lastWeiboId[room.Key] = id;
+                                    continue;
+                                }
+                                else if (_lastWeiboId[room.Key] == id)
+                                {
+                                    continue;
+                                }
+
+                                _lastWeiboId[room.Key] = id;
+                                _logger.LogInformation("weibo[{0}] start sending notice", room.Key);
+
+                                text = _faceRegex.Replace(text, "$1");
+                                text = _newLineRegex.Replace(text, Environment.NewLine);
+                                text = _fullTextRegex.Replace(text, "[完整内容见原微博](https://m.weibo.cn$1)");
+
+                                string msg = $"【{name}】发布了微博：{Environment.NewLine}{text}";
+
+                                foreach (var sor in room.Value)
+                                {
+                                    var sorModel = sor.Value;
+                                    if (sorModel.Source == Models.Enum.EnumKookMessageSource.Guild)
+                                        await _kookService.SendToGuild(msg, ulong.Parse(sorModel.SourceId));
+                                    else if (sorModel.Source == Models.Enum.EnumKookMessageSource.Direct)
+                                        await _kookService.SendToUser(msg, Guid.Parse(sorModel.SourceId));
+                                    else
+                                        continue;
+
+                                    await Task.Delay(10000);
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                _logger.LogError(e, "weibo failed to send msg");
+                            }
+                            finally
+                            {
+                                await Task.Delay(intervalSpan);
+                            }
+                        }
+                    }
+                    await Task.WhenAny(Task.Delay(loopSpan), _completionSource.Task);
+                }
+                _logger.LogInformation("loop finished");
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "weibo loop error");
+            }
+
+        }
+
+
+        internal enum StreamType
+        {
+            PC = 0,
+            Radio = 1
+        }
+    }
+}
---
+++ src/GensouSakuya.KookBot/GensouSakuya.KookBot.App/Handlers/WeiboHandler.cs
@@ -0,0 +1,125 @@
+﻿using GensouSakuya.KookBot.App.BackgroundWorkers;
+using GensouSakuya.KookBot.App.Handlers.Base;
+using GensouSakuya.KookBot.App.Interfaces;
+using GensouSakuya.KookBot.App.Models;
+using GensouSakuya.KookBot.App.Models.Enum;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+
+namespace GensouSakuya.KookBot.App.Handlers
+{
+    [CommandTrigger("weibo")]
+    internal class WeiboHandler : BaseDataHandler<ConcurrentDictionary<string, ConcurrentDictionary<string, WeiboSubscribeModel>>>
+    {
+        private WeiboWorker _worker;
+        public WeiboHandler(IDataService dataService, IKookService kookService, ILogger logger) : base(dataService,kookService, logger)
+        {
+            _worker = new WeiboWorker(kookService, logger, GetData);
+        }
+
+        public override Task<bool> Check(EnumKookMessageSource source, string? originMessage, BaseKookUser sendBy, BaseKookChannel sendFrom)
+        {
+            return Task.FromResult(true);
+        }
+
+        public override async Task NextAsync(EnumKookMessageSource source, string? originMessage, BaseKookUser sendBy, BaseKookChannel sendFrom)
+        {
+            WeiboSubscribeModel sbm;
+            if (source == EnumKookMessageSource.Guild)
+            {
+                //if (sendBy != admin)
+                //{
+                //    MessageManager.SendToSource(source, "目前只有机器人管理员可以配置该功能哦");
+                //    return;
+                //}
+
+                sbm = new WeiboSubscribeModel
+                {
+                    Source = EnumKookMessageSource.Guild,
+                    SourceId = sendFrom.ToGuildChannel().Id.ToString()
+                };
+            }
+            else if(source == EnumKookMessageSource.Direct)
+            {
+                sbm = new WeiboSubscribeModel
+                {
+                    Source = EnumKookMessageSource.Direct,
+                    SourceId = sendFrom.ToDMChannel().ChatCode.ToString()
+                };
+            }
+            else
+            {
+                await SendBack(source, "懒得支持！", sendFrom);
+                return;
+            }
+
+            var commands = MessageToCommand(originMessage);
+            if (commands.Count < 3)
+            {
+                return;
+            }
+
+            var first = commands[1];
+            var roomId = commands[2];
+
+            var subscribers = GetData();
+            if (first == "subscribe")
+            {
+                var sub = subscribers.GetOrAdd(roomId, new ConcurrentDictionary<string, WeiboSubscribeModel>());
+                if (sub.ContainsKey(sbm.ToString()))
+                {
+                    await SendBack(source, "该微博已订阅", sendFrom);
+                    return;
+                }
+
+                sub[sbm.ToString()] = sbm;
+                await SendBack(source, "订阅成功！", sendFrom);
+                SaveData(subscribers);
+            }
+            else if (first == "unsubscribe")
+            {
+                if (!subscribers.TryGetValue(roomId, out var sub))
+                {
+                    return;
+                }
+                if(sub == null)
+                {
+                    subscribers.TryRemove(roomId, out _);
+                    SaveData(subscribers);
+                    return;
+                }
+                if (sub.Remove(sbm.ToString()!, out _))
+                {
+                    await SendBack(source, "取消订阅成功！", sendFrom);
+                    SaveData(subscribers);
+                }
+                return;
+            }
+
+            return;
+        }
+    }
+
+    internal class WeiboSubscribeModel
+    {
+        public EnumKookMessageSource Source { get; set; }
+        public required string SourceId { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is WeiboSubscribeModel model &&
+                   Source == model.Source &&
+                   SourceId == model.SourceId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Source, SourceId);
+        }
+
+        public override string ToString()
+        {
+            return $"{Source}:{SourceId}";
+        }
+    }
+}
